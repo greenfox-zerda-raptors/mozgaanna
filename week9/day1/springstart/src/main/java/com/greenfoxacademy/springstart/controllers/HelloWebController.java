092a125437bc413b924c127d4f6157d9af31f3d1
Mozga Anna <mozgaanna@gmail.com>
2016-12-19 14:20:25 +0100

    Web Greet counter app

--- week9/day1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week9/day1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -4,15 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by mozgaanna on 19/12/16.
  */
 @Controller
 public class HelloWebController {
+    AtomicLong atomicLong = new AtomicLong(1);
 
     @RequestMapping("/web/greeting")
     public String greeting(Model model) {
         model.addAttribute("name", " World");
+        model.addAttribute("counter", " Number of reload: " + atomicLong.getAndIncrement());
         return "greeting";
     }
 }
