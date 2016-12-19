fa9f36450864e86859dc014dc7a6dbd3fe6f78d3
Mozga Anna <mozgaanna@gmail.com>
2016-12-19 13:54:10 +0100

    REST Greet counter App done

--- week9/day1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week9/day1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by mozgaanna on 19/12/16.
  */
@@ -11,11 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class HelloRESTController {
 
+    AtomicLong atomicLong = new AtomicLong(1);
     @RequestMapping(value = "/greeting")
 
+
    public Greeting greeting(@RequestParam("name") String name){
-        Greeting greeting = new Greeting("Hello, " + name, 1);
+
+        Greeting greeting = new Greeting("Hello, " + name, atomicLong.getAndIncrement());
         return greeting;
+
+
    }
 
 }
