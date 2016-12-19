1714b137db156d182ec68a451649d6479ac8b022
Mozga Anna <mozgaanna@gmail.com>
2016-12-19 15:04:15 +0100

    Saying Hello to the Whole World done

--- week9/day1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week9/day1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -19,4 +19,16 @@ public class HelloWebController {
         model.addAttribute("counter", " Number of reload: " + atomicLong.getAndIncrement());
         return "greeting";
     }
+
+
+    @RequestMapping("/web/helloeverybody")
+    public String helloEverybody(Model model) {
+        String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+                "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+                "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+                "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+        model.addAttribute("hellos", hellos);
+        return "helloEverybody";
+    }
 }
